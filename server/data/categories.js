be06dcffa9089ca7e3119b5115cf056aae4bcf65
Hazem Montaser <hazemmontaser@gmail.com>
2026-09-21 01:38:14 +0300

    إضافة الكتالوج المصري الشامل لجميع أنواع الشيبسي والسناكس والمشروبات الغازية واللبان والحلويات

---
+++ server/data/categories.js
@@ -0,0 +1,12 @@
+module.exports = [
+  { name: 'سجائر وتبغ', code: 'TOBACCO', icon: 'Flame', sort_order: 1 },
+  { name: 'شيبسي ومقرمشات', code: 'SNACKS', icon: 'Sparkles', sort_order: 2 },
+  { name: 'مشروبات وغازيات', code: 'DRINKS', icon: 'Coffee', sort_order: 3 },
+  { name: 'لبان وحلويات وبسكويت', code: 'SWEETS', icon: 'Cookie', sort_order: 4 },
+  { name: 'ألبان وأجبان', code: 'DAIRY', icon: 'Milk', sort_order: 5 },
+  { name: 'مشروبات ساخنة وبن', code: 'HOT_DRINKS', icon: 'Cup', sort_order: 6 },
+  { name: 'سلع تموينية وبقوليات', code: 'GROCERY', icon: 'ShoppingBag', sort_order: 7 },
+  { name: 'معلبات وتونة وصوصات', code: 'CANNED', icon: 'Package', sort_order: 8 },
+  { name: 'منظفات وورقيات', code: 'CLEANING', icon: 'Sparkle', sort_order: 9 },
+  { name: 'عناية شخصية', code: 'PERSONAL_CARE', icon: 'Heart', sort_order: 10 }
+];
